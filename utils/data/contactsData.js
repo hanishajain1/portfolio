c1b1e011a944a9706e950a74dd6eb3ae1d72533e
Hanisha Jain <75520644+hanisha27@users.noreply.github.com>
2024-10-15 17:14:19 +0530

    remove faceebook and unwanted

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,7 +3,7 @@ export const contactsData = {
     phone: '+8801608797655',
     address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
     // github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    // facebook: 'https://www.facebook.com/abusaid.riyaz/',
     linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
     twitter: 'https://x.com/hanisshajaiin',
     // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
